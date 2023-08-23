c32ca679f9d0f501b3d96b79cd3bbc53b9db65a9
ervnn <ervinsetyanata5@gmail.com>
2023-08-23 14:44:40 +0700

    ganti ngadu.id -> MaduloRek

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login Ngadu.id</title>
+	<title>Login MaduloRek</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -27,7 +27,7 @@
 				<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
                     @csrf
 					<span class="login100-form-title">
-						Selamat Datang di Ngadu.id
+						Selamat Datang di MaduloRek
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
