0c4eb525a058c751eb405ee84af906bfc897c9b2
ervnn <ervinsetyanata5@gmail.com>
2023-08-23 11:29:42 +0700

    first-commit

---
+++ resources/views/includes/admin/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; DarkSide.</strong>
+    All rights reserved.
+  </footer>
---
+++ resources/views/pages/admin/dashboard.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.admin')
+
+@section('title')
+Dashboard
+@endsection
+
+@section('content')
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+
+<section class="content">
+<div class="container-fluid">
+<div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{ $pengaduan }}</h3>
+
+                <p>Jumlah Pengaduan</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>{{ $success }}</h3>
+
+                <p>Sudah Selesai</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>{{ $process }}</h3>
+
+                <p>Sedang dalam Pemrosesan</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{ $pending }}</h3>
+
+                <p>Belum ada Pemrosesan</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        </section>
+        </div>
+@endsection
